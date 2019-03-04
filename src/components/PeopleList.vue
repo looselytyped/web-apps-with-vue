8cd383ee4a44e496ceae36c460c0a9ed64fa14e0
Raju Gandhi <raju.gandhi@gmail.com>
2019-03-04 18:52:42 -0500

    feat: use axios to retrieve all the friends

--- src/components/PeopleList.vue
+++ src/components/PeopleList.vue
@@ -23,37 +23,9 @@
 
 <script>
 import PersonItem from "./PersonItem";
+import axios from "axios";
 
-const friends = [
-  {
-    id: 1,
-    firstName: "Michelle",
-    lastName: "Mulroy",
-    gender: "female",
-    fav: true
-  },
-  {
-    id: 2,
-    firstName: "Venkat",
-    lastName: "Subramanian",
-    gender: "male",
-    fav: true
-  },
-  {
-    id: 3,
-    firstName: "Matt",
-    lastName: "Forsythe",
-    gender: "none",
-    fav: false
-  },
-  {
-    id: 4,
-    firstName: "Nate",
-    lastName: "Schutta",
-    gender: "male",
-    fav: false
-  }
-];
+const friends = [];
 
 export default {
   components: {
@@ -68,6 +40,11 @@ export default {
     like(friend) {
       friend.fav = !friend.fav;
     }
+  },
+  mounted() {
+    axios.get("http://localhost:3000/friends").then(response => {
+      this.friends = response.data;
+    });
   }
 };
 </script>
