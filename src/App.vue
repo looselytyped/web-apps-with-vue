4211172ea101d4bfbebfe541053f3bd07ed60bbd
Raju Gandhi <raju.gandhi@gmail.com>
2019-03-05 15:04:20 -0500

    feat: flesh out the layout of the application

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,84 @@
 <template>
-  <div id="app">
-    Hello!
-  </div>
+  <!-- Blatantly stolen from https://vuetifyjs.com/en/examples/layouts/sandbox -->
+  <v-app id="sandbox">
+    <v-navigation-drawer
+      v-model="primaryDrawer.model"
+      :permanent="primaryDrawer.type === 'permanent'"
+      :temporary="primaryDrawer.type === 'temporary'"
+      absolute
+      overflow
+      app
+    >
+      <div class="pa-3 text-xs-center teal white--text">
+        <div class="display-2 py-4">
+          Friends HQ
+        </div>
+        <p>Together, we are stronger</p>
+      </div>
+      <v-spacer></v-spacer>
+      <v-list dense>
+        <template v-for="(item, i) in items">
+          <v-divider dark v-if="item.divider" class="my-3" :key="i"></v-divider>
+          <v-list-tile :key="i" v-else>
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title class="grey--text">
+                {{ item.text }}
+              </v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app absolute>
+      <v-toolbar-side-icon
+        v-if="primaryDrawer.type !== 'permanent'"
+        @click.stop="primaryDrawer.model = !primaryDrawer.model"
+      ></v-toolbar-side-icon>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid>
+        <v-layout align-center justify-center>
+          <v-flex xs10>
+            <v-card>
+              <v-card-text>
+                <v-layout row wrap>
+                  <hello-world></hello-world>
+                </v-layout>
+              </v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer :inset="true" app>
+      <span class="px-3">
+        &copy; Looselytyped {{ new Date().getFullYear() }}
+      </span>
+    </v-footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+<script>
+import HelloWorld from "./components/HelloWorld";
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  components: {
+    HelloWorld
+  },
+  data: () => ({
+    primaryDrawer: {
+      model: null,
+      type: "default (no property)"
+    },
+    items: [
+      { icon: "dashboard", text: "Dashboard" },
+      { icon: "contacts", text: "Contacts" },
+      { divider: true },
+      { icon: "notes", text: "Journal" }
+    ]
+  })
+};
+</script>
