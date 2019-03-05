8617ad21f4a8c4ab61f363616cdb6bd66ff25848
Raju Gandhi <raju.gandhi@gmail.com>
2019-03-05 16:15:45 -0500

    fix: remove unnecessary files till we need them

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,27 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld />
-    </v-main>
-  </v-app>
+  <div id="app">
+    Hello!
+  </div>
 </template>
 
-<script>
-import HelloWorld from "./components/HelloWorld";
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
 
-export default {
-  name: "App",
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
 
-  components: {
-    HelloWorld
-  },
-
-  data: () => ({
-    //
-  })
-};
-</script>
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
