9011d8aecc221cc8befcc22e9858a39c863fbca3
Raju Gandhi <raju.gandhi@gmail.com>
2019-03-05 16:15:45 -0500

    fix: remove unnecessary files till we need them

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,27 @@
 <template>
-  <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld />
-    </v-content>
-  </v-app>
+  <div id="app">
+    Hello!
+  </div>
 </template>
 
-<script>
-import HelloWorld from "./components/HelloWorld";
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
 
-export default {
-  name: "App",
-  components: {
-    HelloWorld
-  },
-  data: () => ({
-    //
-  })
-};
-</script>
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
