23d9fa1503a921c154abb583a4ce640566fe7ea5
Raju Gandhi <raju.gandhi@gmail.com>
2019-10-13 17:55:03 -0400

    feat: extract PeopleList into a separate component and make People a view

--- src/components/PeopleList.vue
+++ src/components/PeopleList.vue
@@ -1,24 +1,22 @@
 <template>
-  <v-container fluid>
-    <v-layout row>
-      <v-flex grow pa-1>
-        <v-card>
-          <v-list header>
-            <PersonItem
-              v-for="(friend, index) in friends"
-              :key="friend.id"
-              :friend="friend"
-              :last="index === friends.length - 1"
-              @friend-liked="like"
-            ></PersonItem>
-          </v-list>
-        </v-card>
-      </v-flex>
-      <v-flex shrink pa-1>
-        <v-btn color="success" dark large>Add Friend</v-btn>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <v-layout row>
+    <v-flex grow pa-1>
+      <v-card>
+        <v-list header>
+          <PersonItem
+            v-for="(friend, index) in friends"
+            :key="friend.id"
+            :friend="friend"
+            :last="index === friends.length - 1"
+            @friend-liked="like"
+          ></PersonItem>
+        </v-list>
+      </v-card>
+    </v-flex>
+    <v-flex shrink pa-1>
+      <v-btn color="success" dark large>Add Friend</v-btn>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
