08eb94c286a25d03a8554ebe84f3f011d7b76107
Raju Gandhi <raju.gandhi@gmail.com>
2019-10-18 18:41:51 -0400

    feat: add Vuex

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import "./plugins/vuetify";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import router from "./router";
+import store from './store'
 
 Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
   router,
+  store,
   render: h => h(App)
 }).$mount("#app");
